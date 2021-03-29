6815bb6a955eeb486c38543be0fc7ecc9d73e111
Alejandra <marialesr.17@gmail.com>
2021-03-29 12:34:33 -0600

    modified model order and routes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,26 @@
 class Order < ApplicationRecord
   belongs_to :user
 
+  validates :number, uniqueness: true 
+
   before_create :generate_number
 
   def generate_number
-    self.number ||= loop do 
-      random = "BO#{Array.new(9){rand(9)}.join }"
+    self.number ||= loop do # Doble paith es or 
+      random = random_candidate
       break random unless self.class.exists?(number: random) 
+    end
+  end
+
+  def random_candidate
+    "#{hash_prefix}#{Array.new(hash_size){rand(hash_size)}.join }"
+  end
+
+  def hash_prefix
+    'BO'
+  end
+
+  def hash_size
+    9
   end
 end
